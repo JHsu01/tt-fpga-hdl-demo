eed28047ac443481203c9410037742e2ed6f430e
Joseph Hsu <54673847+JHsu01@users.noreply.github.com>
2024-02-16 09:17:40 -0500

    Update top.sv
    Clock time 11

--- src/top.sv
+++ src/top.sv
@@ -283,7 +283,7 @@ logic [3:0] FpgaPins_Fpga_CLOCK_TIME_tens_digit_a0,
             
             
                      // $refresh = LED frequency of switching
-                     assign FpgaPins_Fpga_CLOCK_TIME_refresh_a0 = (FpgaPins_Fpga_CLOCK_TIME_cycounter_a0 == 24'd20000 - 24'd1);
+                     assign FpgaPins_Fpga_CLOCK_TIME_refresh_a0 = (FpgaPins_Fpga_CLOCK_TIME_cycounter_a0 == 24'd60000 - 24'd1);
             
                      assign FpgaPins_Fpga_CLOCK_TIME_ones_digit_a0[3:0] = (FpgaPins_Fpga_CLOCK_TIME_reset_a0) ? 4'b0:
                                          !FpgaPins_Fpga_CLOCK_TIME_pulse_a0 ? FpgaPins_Fpga_CLOCK_TIME_ones_digit_a1 :
@@ -296,15 +296,15 @@ logic [3:0] FpgaPins_Fpga_CLOCK_TIME_tens_digit_a0,
                                         (FpgaPins_Fpga_CLOCK_TIME_ones_digit_a1 == 4'b1001) ? FpgaPins_Fpga_CLOCK_TIME_tens_digit_a1 + 1 :
                                         FpgaPins_Fpga_CLOCK_TIME_tens_digit_a1;
             
-                     assign FpgaPins_Fpga_CLOCK_TIME_show_tens_a0 = (FpgaPins_Fpga_CLOCK_TIME_reset_a0) ? 1'b0 :
+                     assign FpgaPins_Fpga_CLOCK_TIME_show_tens_a0 = (FpgaPins_Fpga_CLOCK_TIME_reset_a0)  ? 1'b0 :
                                    FpgaPins_Fpga_CLOCK_TIME_refresh_a0 ? !FpgaPins_Fpga_CLOCK_TIME_show_tens_a1 :
-                                   FpgaPins_Fpga_CLOCK_TIME_show_tens_a1;
+                                              FpgaPins_Fpga_CLOCK_TIME_show_tens_a1;
             
             
                      assign FpgaPins_Fpga_CLOCK_TIME_digit_a0[3:0] = FpgaPins_Fpga_CLOCK_TIME_show_tens_a0 ? FpgaPins_Fpga_CLOCK_TIME_tens_digit_a0:
-                              FpgaPins_Fpga_CLOCK_TIME_ones_digit_a0;
-                     assign uo_out[7] = !FpgaPins_Fpga_CLOCK_TIME_show_tens_a0;
+                                                FpgaPins_Fpga_CLOCK_TIME_ones_digit_a0;
             
+                     assign uo_out[7] = !FpgaPins_Fpga_CLOCK_TIME_show_tens_a0;
                      assign uo_out[6:0] =
                         (FpgaPins_Fpga_CLOCK_TIME_digit_a0 == 4'h00) ? 7'b0111111 :
                         (FpgaPins_Fpga_CLOCK_TIME_digit_a0 == 4'h01) ? 7'b0000110 :
